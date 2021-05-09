7e2f45184c959994ef5939d33fe3a3e137cd5dcc
Chris <chris.whalen37@gmail.com>
2021-05-09 17:26:58 -0600

    added more functionality and everything is functioning appropriately

--- db/index.js
+++ db/index.js
@@ -2,16 +2,39 @@ const connection = require('./connection');
 
 class DB {
 
-    constructor(connection){
+    constructor(connection) {
         this.connection = connection
     }
 
-    viewAllEmp(){
+    viewAllEmp() {
         return this.connection.query('SELECT * FROM employee;');
     }
-    
+
+    viewAllPositions() {
+        return this.connection.query('SELECT * FROM position;');
+    }
+
+    viewAllDept() {
+        return this.connection.query('SELECT * FROM department;');
+    }
+
+    addEmp(employee) {
+        return this.connection.query('INSERT INTO employee SET ?', employee)
+    }
+
+    addPosition(position) {
+        return this.connection.query('INSERT INTO position SET ?', position)
+    }
+
     addDept(department) {
         return this.connection.query('INSERT INTO department SET ?', department)
+    }
+
+    updateEmp(employeeId, positionId) {
+        return this.connection.query(
+            "UPDATE employee SET position_id = ? WHERE id = ?;",
+            [positionId, employeeId]
+        );
     }
 }
 
--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const start = async () => {
         name: 'question',
         type: 'list',
         message: 'What would you like to do?',
-        choices: ['View all employees', 'View all positions', 'View all departments', 'Add employee', 'Add postion', 'Add department', 'Update employee role', 'Exit'],
+        choices: ['View all employees', 'View all positions', 'View all departments', 'Add employee', 'Add position', 'Add department', 'Update employee role', 'Exit'],
     })
     switch (question) {
         case 'View all employees':
             viewAllEmp();
             break;
-        case 'View all postions':
+        case 'View all positions':
             viewAllPositions();
             break;
         case 'View all departments':
@@ -38,27 +38,33 @@ const start = async () => {
 };
 
 const viewAllEmp = async () => {
-    try{
+    try {
         const employees = await db.viewAllEmp();
         console.log('\n');
         console.table(employees);
         start();
     }
-    catch(err){console.log(err)}
+    catch (err) { console.log(err) }
 };
 
 const viewAllPositions = async () => {
-    const position = await db.viewAllPostions();
-    console.log('\n');
-    console.table(position);
-    start();
+    try {
+        const position = await db.viewAllPositions();
+        console.log('\n');
+        console.table(position);
+        start();
+    }
+    catch (err) { console.log(err) }
 };
 
 const viewAllDept = async () => {
-    const departments = await db.viewAllDept();
-    console.log('\n');
-    console.table(departments);
-    start();
+    try {
+        const departments = await db.viewAllDept();
+        console.log('\n');
+        console.table(departments);
+        start();
+    }
+    catch (err) { console.log(err) }
 };
 
 const addEmp = async () => {
@@ -81,14 +87,14 @@ const addEmp = async () => {
         value: id,
     }));
 
-    const { jobId } = await prompt({
+    const { positionId } = await prompt({
         type: "list",
-        name: "jobId",
+        name: "position_id",
         message: "What is the new employee's position?",
         choices: positionChoices,
     });
 
-    employee.job_id = jobId
+    employee.position_id = positionId
 
     const managerChoices = employees.map(({ id, first_name, last_name }) => ({
         name: `${first_name} ${last_name}`,
@@ -99,7 +105,7 @@ const addEmp = async () => {
 
     const { managerId } = await prompt({
         type: "list",
-        name: "managerId",
+        name: "manager_id",
         message: "Who is the new employee's manager?",
         choices: managerChoices,
     });
@@ -113,7 +119,21 @@ const addEmp = async () => {
 };
 
 const addPosition = async () => {
-    
+    const position = await prompt([
+        {
+            name: 'title',
+            message: "What is the title of the new position?",
+        },
+        {
+            name: 'salary',
+            message: "What is the salary of the new position?",
+        },
+    ]);
+
+    await db.addPosition(position);
+    console.log(`added ${position.name} as new position`);
+
+    start();
 }
 
 const addDept = async () => {
@@ -124,16 +144,45 @@ const addDept = async () => {
 
     await db.addDept(department);
     console.log(`added ${department.name} as new department`);
-    
+
     start();
 }
 
 const updateEmp = async () => {
+    const employees = await db.viewAllEmp();
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+    }));
+
+    const { employeeId } = await prompt({
+        name: 'employeeId',
+        type: 'list',
+        message: 'Which employee will be udated?',
+        choices: employeeChoices
+    });
+
+    const position = await db.viewAllPositions()
+
+    const roleChoices = position.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+
+    const { positionId } = await prompt({
+        name: 'positionId',
+        type: 'list',
+        message: 'What role will this employee have?',
+        choices: roleChoices
+    });
+    await db.updateEmp(employeeId, positionId);
+    console.log('Updated employees role.')
+    start()
 
 }
 
 const quit = () => {
-    console.log('Goodbye')
+    console.log('Ok BYE!')
     process.exit();
 }
 
